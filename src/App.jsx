1cef8bf1337127f2935c14aa90ad1ab229bd1e48
DavidLopF <davidtach26@gmail.com>
2025-10-05 10:55:21 -0500

    molto catalog

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import TVShowcase from './components/TVShowcase'
 import TabletShowcase from './components/TabletShowcase'
-import Home from './components/Home'
 import products from './data/products.json'
 
 export default function App() {
@@ -21,38 +19,6 @@ export default function App() {
             primaryColor="#f6e8d6"
           />
         } />
-        <Route path="/home" element={<Home />} />
-        
-        {/* Modo TV - Para televisores y pantallas grandes */}
-        <Route 
-          path="/tv" 
-          element={
-            <TVShowcase
-              products={products}
-              brand="Molto"
-              whatsapp="573001112233"
-              instagram="@mi_marca"
-              qrSrc="/qr-placeholder.svg"
-              slideMs={7000}
-            />
-          } 
-        />
-        
-        {/* Modo Tablet - Para ferias y eventos */}
-        <Route 
-          path="/tablet" 
-          element={
-            <TabletShowcase
-              products={products}
-              brand="Molto"
-              whatsapp="321 225 4230"
-              instagram="@molto_co"
-              qrSrc="https://i.ibb.co/gphnJKq/molto-co-qr-1.png"
-              autoSlideMs={6000}
-              primaryColor="#f6e8d6"
-            />
-          } 
-        />
       </Routes>
     </Router>
   )
